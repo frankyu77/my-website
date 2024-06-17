47126ff3577a04544d25349ffd1b0763d4cb323f
Frank Yu <kyu16@students.cs.ubc.ca>
2024-06-17 09:48:30 -0700

    started working on second project

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Title from './components/Title'
 import Dropdown from './components/Dropdown';
-import Accordian from './components/Accordian';
 
 function App() {
   return (
     <div className="App">
       <Title />
       <Dropdown />
-      <Accordian />
     </div>
   );
 }
